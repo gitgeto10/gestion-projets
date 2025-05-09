14163e9fd9d5daf54e8bbea3201879e4de06e566
Aitelbourane_oumaima <oumaimaaitelbourane@gmail.com>
2025-05-10 00:09:40 +0200

    Initial commit

---
+++ src/main/java/com/mycompany/gestion/projets/InitDbApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.mycompany.gestion.projets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitDbApp {
+    public static void main(String[] args) {
+        SpringApplication.run(InitDbApp.class, args);
+    }
+}
